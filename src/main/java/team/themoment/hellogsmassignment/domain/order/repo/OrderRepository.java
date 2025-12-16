c9a0edfceb2cf2debae70e5b40c56d6d03cc2874
hongjm0912~ <s25036@gsm.hs.kr>
2025-12-16 15:02:37 +0900

    refactor: 미사용 메서드 주석처리

--- src/main/java/team/themoment/hellogsmassignment/domain/order/repo/OrderRepository.java
+++ src/main/java/team/themoment/hellogsmassignment/domain/order/repo/OrderRepository.java
@@ -11,31 +11,32 @@ import team.themoment.hellogsmassignment.domain.order.entity.type.OrderStatus;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    @Query("SELECT o FROM Order o " +
-            "WHERE (:status IS NULL OR o.status = :status) " +
-            "AND (:minPrice IS NULL OR o.totalPrice >= :minPrice) " +
-            "AND (:maxPrice IS NULL OR o.totalPrice <= :maxPrice) " +
-            "AND (:startDate IS NULL OR o.createdTime >= :startDate) " +
-            "AND (:endDate IS NULL OR o.createdTime <= :endDate)")
-    Page<Order> searchOrders(@Param("status") OrderStatus status,
-                             @Param("minPrice") BigDecimal minPrice,
-                             @Param("maxPrice") BigDecimal maxPrice,
-                             @Param("startDate") LocalDateTime startDate,
-                             @Param("endDate") LocalDateTime endDate,
-                             Pageable pageable);
+public interface OrderRepository extends JpaRepository<Order, Long>, OrderRepositoryCustom {
 
-    @Query("SELECT count(*) FROM Order o " +
-            "WHERE (:status IS NULL OR o.status = :status) " +
-            "AND (:minPrice IS NULL OR o.totalPrice >= :minPrice) " +
-            "AND (:maxPrice IS NULL OR o.totalPrice <= :maxPrice) " +
-            "AND (:startDate IS NULL OR o.createdTime >= :startDate) " +
-            "AND (:endDate IS NULL OR o.createdTime <= :endDate)")
-    int countSearchOrder(@Param("status") OrderStatus status,
-                             @Param("minPrice") BigDecimal minPrice,
-                             @Param("maxPrice") BigDecimal maxPrice,
-                             @Param("startDate") LocalDateTime startDate,
-                             @Param("endDate") LocalDateTime endDate);
+//    @Query("SELECT o FROM Order o " +
+//            "WHERE (:status IS NULL OR o.status = :status) " +
+//            "AND (:minPrice IS NULL OR o.totalPrice >= :minPrice) " +
+//            "AND (:maxPrice IS NULL OR o.totalPrice <= :maxPrice) " +
+//            "AND (:startDate IS NULL OR o.createdTime >= :startDate) " +
+//            "AND (:endDate IS NULL OR o.createdTime <= :endDate)")
+//    Page<Order> searchOrders(@Param("status") OrderStatus status,
+//                             @Param("minPrice") BigDecimal minPrice,
+//                             @Param("maxPrice") BigDecimal maxPrice,
+//                             @Param("startDate") LocalDateTime startDate,
+//                             @Param("endDate") LocalDateTime endDate,
+//                             Pageable pageable);
+//
+//    @Query("SELECT count(*) FROM Order o " +
+//            "WHERE (:status IS NULL OR o.status = :status) " +
+//            "AND (:minPrice IS NULL OR o.totalPrice >= :minPrice) " +
+//            "AND (:maxPrice IS NULL OR o.totalPrice <= :maxPrice) " +
+//            "AND (:startDate IS NULL OR o.createdTime >= :startDate) " +
+//            "AND (:endDate IS NULL OR o.createdTime <= :endDate)")
+//    int countSearchOrder(@Param("status") OrderStatus status,
+//                             @Param("minPrice") BigDecimal minPrice,
+//                             @Param("maxPrice") BigDecimal maxPrice,
+//                             @Param("startDate") LocalDateTime startDate,
+//                             @Param("endDate") LocalDateTime endDate);
 
 }
